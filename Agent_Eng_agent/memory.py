259bbcc08e3e469dbe1814829be7ea4f58f56767
ffflzzz <abc660341@gmail.com>
2025-05-27 22:28:54 +0800

    add a new project[Agent_Eng]

---
+++ Agent_Eng_agent/memory.py
@@ -0,0 +1,11 @@
+from langchain.memory import ConversationBufferMemory
+
+def memories():
+    memory = ConversationBufferMemory(    
+    memory_key="history",  # 存对话的变量名，要和prompt里的{history}一致！
+    input_key="input",     # 用户输入的变量名（对应prompt里的{input}）
+    output_key="response", # 智能体输出的变量名（可选，不写默认用"text"）
+    return_messages=True,
+    k=10,  # 保留最近的10条对话记录，不写默认无限长（会爆显存）
+    )  
+    return memory
